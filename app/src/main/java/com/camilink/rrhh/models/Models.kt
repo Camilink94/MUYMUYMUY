c037e7b3c49d0645aaaa1f293cb6b68441a10334
Camilink <camilo.chaparro.co@gmail.com>
2020-01-24 15:40:19 -0500

    feat: Add Room and Employee model

---
+++ app/src/main/java/com/camilink/rrhh/models/Models.kt
@@ -0,0 +1,15 @@
+package com.camilink.rrhh.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class EmployeeModel(
+    @PrimaryKey val id: Int, val name: String = "",
+    val position: String = "",
+    val salary: String = "",
+    val phone: String = "",
+    val email: String = "",
+    @ColumnInfo(name = "upper_relation") val upperRelation: Int = 0
+)
