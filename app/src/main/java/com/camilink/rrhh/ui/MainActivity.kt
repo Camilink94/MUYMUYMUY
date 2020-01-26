2aeba8797793d7beaaad91c12b8a53de17e1530b
Camilink <camilo.chaparro.co@gmail.com>
2020-01-26 18:22:53 -0500

    feat: Implement presenter in MainActivity

--- app/src/main/java/com/camilink/rrhh/ui/MainActivity.kt
+++ app/src/main/java/com/camilink/rrhh/ui/MainActivity.kt
@@ -2,22 +2,46 @@ package com.camilink.rrhh.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.ui.AppBarConfiguration
+import android.util.Log
 import com.camilink.rrhh.R
+import com.camilink.rrhh.models.EmployeeModel
+import com.camilink.rrhh.presenter.EmployeePresenterContract
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
 
 class MainActivity : AppCompatActivity(),
+    KoinComponent,
+    EmployeePresenterContract.IView,
+    //Fragments
     ListFragment.Listener,
     NewEmployeesFragment.Listener,
     DetailFragment.Listener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration;
+    private val presenter: EmployeePresenterContract.IPresenter by inject { parametersOf(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        presenter.getLatestEmployees()
+
     }
+
+    //region View
+    override fun setLatestEmployees(employees: ArrayList<EmployeeModel>) {
+        Log.d("AAAA", "Recieved ${employees.size} employees:\n$employees")
+    }
+
+    override fun connError() {
+
+    }
+
+    override fun dataError() {
+
+    }
+    //endregion
 
 }
