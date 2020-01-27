a4454d6f6a820f9b0e099108f66575577e5767e7
Camilink <camilo.chaparro.co@gmail.com>
2020-01-27 03:32:36 -0500

    feat: Add Toolbar animations with nav controller

--- app/src/main/java/com/camilink/rrhh/ui/MainActivity.kt
+++ app/src/main/java/com/camilink/rrhh/ui/MainActivity.kt
@@ -2,7 +2,12 @@ package com.camilink.rrhh.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.camilink.rrhh.R
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.core.KoinComponent
 
 class MainActivity : AppCompatActivity(),
@@ -17,10 +22,19 @@ class MainActivity : AppCompatActivity(),
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 
-    //region ListFragment Listener
+    fun showLoading() {
+        loadingGr.visibility = View.VISIBLE
+    }
 
-    //endregion
+    fun hideLoading() {
+        loadingGr.visibility = View.GONE
+    }
 
 }
